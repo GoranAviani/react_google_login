3de6b8ba29ae19d58ef2773139390cca8d19435c
goranaviani <goranaviani@gmail.com>
2022-06-26 21:02:38 +0200

    google login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import GoogleLoginComponent from "./GoogleLoginComponent";
 function App() {
   return (
     <div className="App">
 <h2>React Google Login Example</h2>
-        <GoogleLoginComponent />
+
     </div>
 
 
