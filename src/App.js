a93c7f9a1b1c27eb22ad786609581bbefffe0faf
goranaviani <goranaviani@gmail.com>
2022-06-26 18:52:46 +0200

    google login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import GoogleSignIn from "./GoogleSignIn";
 
 function App() {
   return (
     <div className="App">
-
+<GoogleSignIn />
     </div>
   );
 }
