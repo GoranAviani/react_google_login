151d27acbb5cc0998bcf64f97b398c9b64eaec9a
goranaviani <goranaviani@gmail.com>
2022-06-29 21:02:54 +0200

    google login

--- src/GoogleLoginService.js
+++ src/GoogleLoginService.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-const googleLogin = async (accesstoken) => {
+const googleLogin = async (response) => {
     let res = await axios.post(
-      "http://localhost:8000/rest-auth/google/",
+      "http://localhost:8000/api/v1/rest-auth/google_auth/",
       {
-        access_token: accesstoken,
+        access_token: response.credential,
+          id_token: response.clientId,
       }
     );
     console.log(res);
